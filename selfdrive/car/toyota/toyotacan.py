5c0641fcd54786bc6df2a453f46e77fe13137363
Kevin <kevinyehdev@gmail.com>
2024-10-04 12:04:31 -0700

    Set barriers status while pressed

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -102,7 +102,7 @@ def create_ui_command(packer, steer, chime, left_line, right_line, left_lane_dep
     "LDA_ALERT": steer,
     "RIGHT_LINE": 3 if right_lane_depart else 1 if right_line else 2,
     "LEFT_LINE": 3 if left_lane_depart else 1 if left_line else 2,
-    "BARRIERS": 1 if lat_active else 0,
+    "BARRIERS": 1 if lat_active and not steeringPressed else 0,
     "LKAS_STATUS": 2 if lat_active else 0,
 
     # static signals
